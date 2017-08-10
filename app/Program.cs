46ac35b244e873be8c72e18e8ec0703b4bfd1f74
Chris McBride <christopher.mcbride@gmail.com>
2017-08-09 22:36:39 -0400

    first commit

---
+++ app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+using Library;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is a new line y...");
+            WriteLine($"The answer is {new Thing().Get(19, 23)}");
+        }
+    }
+}
